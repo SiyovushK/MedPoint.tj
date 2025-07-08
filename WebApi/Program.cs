bcf9d1d362408df9da2692730f52f202bc6043e4
Siyovush <kurbonovs397@gmail.com>
2025-07-09 00:05:01 +0500

    Update Program.cs

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -10,7 +10,7 @@ using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// builder.WebHost.UseUrls("http://147.45.146.15:5063");
+builder.WebHost.UseUrls("http://147.45.146.15:5063");
 
 builder.Services.AddCors(options =>
 {
