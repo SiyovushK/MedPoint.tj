2e251aefa8fb42dac73e3f16ab3401429b457858
SiyovushK <kurbonovs397@gmail.com>
2025-07-25 14:02:14 +0500

    Controller fix

--- WebApi/Controllers/DoctorScheduleController.cs
+++ WebApi/Controllers/DoctorScheduleController.cs
@@ -10,48 +10,53 @@ namespace WebApi.Controllers;
 
 [ApiController]
 [Route("api/[controller]")]
-[Authorize(Roles = $"{Roles.Doctor}, {Roles.Admin}")]
 public class DoctorScheduleController(IDoctorScheduleService scheduleService) : ControllerBase
 {
     [HttpPost]
+    [Authorize(Roles = $"{Roles.Doctor}, {Roles.Admin}")]
     public async Task<ActionResult<Response<GetDoctorScheduleDTO>>> CreateAsync(CreateDoctorScheduleDTO dto)
     {
         var result = await scheduleService.CreateAsync(dto);
-        return StatusCode((int)result.StatusCode, result); 
+        return StatusCode((int)result.StatusCode, result);
     }
 
     [HttpPut]
+    [Authorize(Roles = $"{Roles.Doctor}, {Roles.Admin}")]
     public async Task<ActionResult<Response<GetDoctorScheduleDTO>>> UpdateAsync(int id, UpdateDoctorScheduleDTO dto)
     {
         var result = await scheduleService.UpdateAsync(id, dto);
-        return StatusCode((int)result.StatusCode, result); 
+        return StatusCode((int)result.StatusCode, result);
     }
 
     [HttpDelete]
+    [Authorize(Roles = $"{Roles.Doctor}, {Roles.Admin}")]
     public async Task<ActionResult<Response<string>>> DeleteAsync(int id)
     {
         var result = await scheduleService.DeleteAsync(id);
-        return StatusCode((int)result.StatusCode, result); 
+        return StatusCode((int)result.StatusCode, result);
     }
 
     [HttpGet("ById")]
+    [Authorize(Roles = $"{Roles.Doctor}, {Roles.Admin}")]
     public async Task<ActionResult<Response<GetDoctorScheduleDTO>>> GetByIdAsync(int id)
     {
         var result = await scheduleService.GetByIdAsync(id);
-        return StatusCode((int)result.StatusCode, result); 
+        return StatusCode((int)result.StatusCode, result);
     }
 
     [HttpGet("By-doctor-id")]
+    [AllowAnonymous]
     public async Task<ActionResult<Response<List<GetDoctorScheduleDTO>>>> GetByDoctorIdAsync(int doctorId)
     {
         var result = await scheduleService.GetByDoctorIdAsync(doctorId);
-        return StatusCode((int)result.StatusCode, result); 
+        return StatusCode((int)result.StatusCode, result);
     }
 
     [HttpGet("All")]
+    [Authorize(Roles = Roles.Admin)]
     public async Task<ActionResult<Response<List<GetDoctorScheduleDTO>>>> GetAllAsync([FromQuery] DoctorScheduleFilter filter)
     {
         var result = await scheduleService.GetAllAsync(filter);
-        return StatusCode((int)result.StatusCode, result); 
+        return StatusCode((int)result.StatusCode, result);
     }
 }
